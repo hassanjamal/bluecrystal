23db87650501fc0f6d26c115abf749ed27f2af4a
hassanjamal <hs.jamal@gmail.com>
2014-08-18 15:27:34 +0530

    branch and user create change 02

--- app/views/admin/branch/create.blade.php
+++ app/views/admin/branch/create.blade.php
@@ -124,7 +124,7 @@
 		<!-- Form Actions -->
 		<div class="form-group">
 			<div class="col-md-offset-2 col-md-10">
-				<element class="btn-cancel btn-warning close_popup">Cancel</element>
+				<element class="btn btn-warning  close_popup">Cancel</element>
 				<button type="submit" class="btn btn-success">OK</button>
 			</div>
 		</div>
